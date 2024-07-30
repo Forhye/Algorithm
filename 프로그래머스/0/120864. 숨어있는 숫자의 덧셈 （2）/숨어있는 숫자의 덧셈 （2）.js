dfd467aa268497473585c4216ab144129a1ed390
G-hye <4_hye@naver.com>
2024-07-30 17:21:40 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,10 +1,4 @@
-function solution(my_string) {
-    let sum = 0;
-    let Newmy_string = my_string.replace(/[A-z]/g, " ").split(' ')
-    for (let i = 0; i < Newmy_string.length; i++) {
-        if (!isNaN(Newmy_string[i])) {
-        sum += Number(Newmy_string[i])
-        }
-    }
-    return sum
+function solution(my_string){
+  return my_string.split(/\D+/).reduce((a,c) => a+Number(c),0)
 }
+solution("aAb1B2cC34oOp")
