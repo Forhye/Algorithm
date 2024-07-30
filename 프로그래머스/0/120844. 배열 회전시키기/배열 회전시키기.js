1ce465ead8060ca807d7109a357f5564616f19ee
G-hye <4_hye@naver.com>
2024-07-30 09:23:34 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,8 @@
+function solution(numbers, direction) {
+    if (direction === "right"){
+        numbers = [numbers.pop(), ...numbers];
+    } else {
+        numbers = [...numbers.slice(1), numbers.shift()]
+    }
+    return numbers
+}
