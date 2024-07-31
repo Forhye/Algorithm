2b8bcd5a17996d470316089528f3678ab92cef66
G-hye <4_hye@naver.com>
2024-07-31 09:24:27 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,8 +1,11 @@
 function solution(numbers, direction) {
     if (direction === "right"){
         numbers = [numbers.pop(), ...numbers];
+        // 배열의 맨 뒤를 삭제한 배열, 과 나머지 배열을 넣어줌
     } else {
         numbers = [...numbers.slice(1), numbers.shift()]
+        // 나머지 배열과 배열의 첫번째를 잘라내고
+        // 배열 맨 앞의 값을 shift 해서 뒤에 넣어준다
     }
     return numbers
 }
