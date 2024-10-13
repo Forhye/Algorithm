287122a0e9a078bef8071baa321ddeaf44916cde
G-hye <4_hye@naver.com>
2024-10-13 18:19:49 +0900

    [level 1] Title: 폰켓몬, Time: 0.76 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,13 +1,5 @@
 function solution(nums) {
-        let answer = 0;
-
     const set = new Set(nums);
     
-    const numsLength = parseInt(nums.length)/2;
-    const setSize = set.size;
-    console.log(numsLength)
-    
-    if(setSize < numsLength) return answer=setSize
-    
-    return answer=numsLength;
+    return set.size < parseInt(nums.length)/2 ? set.size : parseInt(nums.length)/2
 }
