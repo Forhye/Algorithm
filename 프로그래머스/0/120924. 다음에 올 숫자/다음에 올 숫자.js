8d858027da51bb3b54a83234c755391849086cad
G-hye <4_hye@naver.com>
2024-11-19 21:51:49 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,7 +1,6 @@
 function solution(common) {
     let answer = 0;
     
-    for(let i = 0 ; i < common.length ; i++){
         let comNum1 = common[1] - common[0]
         let comNum2 = common[2] - common[1]
         
@@ -11,6 +10,6 @@ function solution(common) {
             let remain = common[2]/common[1]
             return answer = common.slice(-1)*remain
         }
-    }
+
     return answer;
 }
