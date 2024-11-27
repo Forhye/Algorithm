db28ef78d4ca2e1bb72d73bdde0d296dbc18fe84
G-hye <4_hye@naver.com>
2024-11-27 20:11:25 +0900

    [level 1] Title: 신고 결과 받기

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,36 +1,32 @@
 function solution(id_list, report, k) {
-    let answer = new Array(id_list.length).fill(0);
+    let answer = new Array(id_list.length).fill(0)
     
-    // 중복 신고 제거
-    let uniqueReports = [...new Set(report)];
+    let duplComplainDelete = [...new Set(report)]
+    let complainArr = []
     
-    // 신고 횟수 카운팅
-    let complainArr = [];
-    uniqueReports.forEach(entry => {
+    duplComplainDelete.forEach(entry => {
         let [complain, badUser] = entry.split(" ");
         complainArr.push(badUser);
     });
     
     const complainCount = complainArr.reduce((acc, item) => {
         acc[item] = (acc[item] || 0) + 1;
-        return acc;
-    }, {});
+        return acc
+    }, {})
     
-    // 정지된 유저 찾기
-    const ban = new Set();
-    for (const [item, count] of Object.entries(complainCount)) {
-        if (count >= k) {
-            ban.add(item);
+    const ban = new Set()
+    for (const [item, count] of Object.entries(complainCount)){
+        if(count >= k){
+            ban.add(item)
         }
     }
     
-    // 메일 발송 횟수 계산
-    uniqueReports.forEach(entry => {
-        let [complain, badUser] = entry.split(" ");
+    duplComplainDelete.forEach(item => {
+        let [complain, badUser] = item.split(" ");
         if (ban.has(badUser)) {
             answer[id_list.indexOf(complain)]++;
         }
-    });
-
+    })
+    
     return answer;
 }
