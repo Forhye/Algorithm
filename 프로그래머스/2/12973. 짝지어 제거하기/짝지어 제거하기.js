8152fbdbe3549d2af27c0566efb64d0d351b820a
G-hye <4_hye@naver.com>
2024-12-20 15:53:09 +0900

    [level 2] Title: 짝지어 제거하기, Time: 57.44 ms, Memory: 50.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,19 @@
+function solution(s)
+{
+    let answer = 0
+    let stack = []
+    
+    for (let char of s) {
+        if (stack.length > 0 && stack[stack.length - 1] === char) {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+    
+    if(stack.length === 0){
+        answer = 1
+    }
+
+    return answer;
+}
